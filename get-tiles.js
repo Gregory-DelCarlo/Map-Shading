fb471a7c2320e164c8ec11620c7f73c6b47fa5cc
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-10-31 13:31:42 -0700

    Added simple mapbox rendering

---
+++ get-tiles.js
@@ -0,0 +1,26 @@
+// require helper functions
+const utils = require("./utils");
+// load mapbox key
+const fs = require("fs");
+const MAPBOX_KEY = fs.readFileSync("map.key").toString();
+
+async function main() {
+    // define zoom and position of map tile
+    const zoom = 10;
+    // somewhere in the grand canyon
+    const [lat, long] = [36.133487, -112.239884];
+
+    // convert to coordinates readable by mapbox
+    const tLat = Math.floor(utils.lat2tile(lat, zoom));
+    const tLong = Math.floor(utils.long2tile(long, zoom));
+
+    // load image from mapbox
+    const image = await utils.loadImage(
+            `https://api.mapbox.com/v4/mapbox.satellite/${zoom}/${tLong}/${tLat}.pngraw?access_token=${MAPBOX_KEY}`
+    );
+
+    // add our tile to the page
+    document.body.appendChild(image);
+}
+
+main();
